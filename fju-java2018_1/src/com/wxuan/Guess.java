4642cec5f18194ba07e5ddcdb868aef6faa177bd
white <white@whiteeat>
2018-11-05 19:42:48 +0800

    10/30

--- fju-java2018_1/src/com/wxuan/Guess.java
+++ fju-java2018_1/src/com/wxuan/Guess.java
@@ -1,9 +1,38 @@
 package com.wxuan;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Guess {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int small = 1, large = 100;
+		Scanner scanner = new Scanner(System.in);
+		float number =(float)Math.random();
+		number = number*100f;
+		int num = (int)number + 1;
+		System.out.println("Guess a number betweem 1 to 100");
+		
+		while(true)
+		{
+			int guess = scanner.nextInt();
+			if(guess > num)
+			{
+				System.out.println("Guess a number" + small + "~" + guess);
+				large = guess; 
+			}
+			else if(guess < num)
+			{
+				System.out.println("Guess a number" + guess + "~" + large);
+				small = guess;
+			}
+			else
+			{
+				System.out.println("°Ú¤£´N¦n´Î´Î");
+				break;
+			}
+		}
 
 	}
 
